74693b04019c113bebc33a2d7865efb601b86e2b
azimjon-95 <>
2025-03-30 07:49:54 +0500

    server 4

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const swaggerOptions = {
         "A comprehensive set of server-side APIs designed for managing operations in a bag manufacturing factory.",
     },
     servers: [
-      { url: "https://bag-manufacturing.vercel.app" },
+      { url: "https://summka-eqz9fcig0-azimjon95s-projects.vercel.app" },
       { url: "http://localhost:5000" },
     ],
   },
@@ -64,51 +64,3 @@ app.use(notfound); // 404 middleware
 
 // Serverni ishga tushirish
 server.listen(PORT, () => console.log(`http://localhost:${PORT}`));
-
-
-
-
-// require("dotenv").config();
-// const express = require("express");
-// const { connect } = require("mongoose");
-// const cors = require("cors");
-// const PORT = process.env.PORT || 5000;
-// const notfound = require("./middleware/notfound.middleware");
-// const router = require("./routes/router");
-// const authMiddleware = require("./middleware/AuthMiddleware");
-
-// const { createServer } = require("node:http");
-// const soket = require("./socket");
-// const app = express();
-// const server = createServer(app);
-// const io = require("./middleware/socket.header")(server);
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// const corsOptions = {
-//   origin: ["http://localhost:3000"],
-//   // origin: "*",
-//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-//   credentials: true,
-// };
-
-// app.use(cors(corsOptions));
-
-// (async () => {
-//   await connect(process.env.MONGO_URI)
-//     .then(() => console.log("MongoDBga ulanish muvaffaqiyatli! ✅✅✅"))
-//     .catch((err) => console.log("MongoDB ulanish xatosi:,🛑🛑🛑", err));
-// })();
-
-// app.set("socket", io);
-// soket.connect(io);
-// app.use(
-//   "/api",
-//   // authMiddleware,
-//   router
-// );
-// app.get("/", (req, res) => res.send("Salom dunyo"));
-// app.use(notfound);
-
-// server.listen(PORT, () => console.log(`http://localhost:${PORT}`));
